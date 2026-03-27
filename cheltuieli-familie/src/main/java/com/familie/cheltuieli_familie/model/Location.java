6a53505864e67573e9217b57f33b4c6d179c36df
EnachescuAlexandra <alexandra.enachescu01@gmail.com>
2026-03-27 15:33:55 +0200

    finished tasks (ExpenseItem entity, updated Location fields and mapped all relations for Expense)

--- cheltuieli-familie/src/main/java/com/familie/cheltuieli_familie/model/Location.java
+++ cheltuieli-familie/src/main/java/com/familie/cheltuieli_familie/model/Location.java
@@ -9,8 +9,10 @@ import lombok.Data;
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    private String name;
-    private String adress;
+    private Long id;
+    private String store;
+    private String city;
+    private String country;
+
 }
