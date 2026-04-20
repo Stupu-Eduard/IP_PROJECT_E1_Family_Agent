7a80bbbaf37482892a5cf529baf3dd65a5fb4294
Miron Andrei <andreimiron486@gmail.com>
2026-04-20 20:20:27 +0300

    Finalizat Geofencng Si Notificari

---
+++ backend/src/main/java/com/familie/cheltuieli_familie/service/MockNotificationService.java
@@ -0,0 +1,16 @@
+package com.familie.cheltuieli_familie.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MockNotificationService implements NotificationProvider {
+
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("\n==========================================");
+        System.out.println("🚨 [ALERTA GEOFENCE]: " + message);
+        System.out.println("📱 [SISTEM]: Trimitere simulată prin Firebase/SMS...");
+        System.out.println("✅ [STATUS]: Notificare livrată cu succes.");
+        System.out.println("==========================================\n");
+    }
+}
