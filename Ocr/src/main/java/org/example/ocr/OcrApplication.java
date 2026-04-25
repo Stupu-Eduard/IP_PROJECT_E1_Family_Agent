cbaa650cedb22d4201660d3af7d4f807803afcd5
FrijaCasian <frijacasian2005@gmail.com>
2026-04-25 18:46:48 +0300

    feat: add OCR module

---
+++ Ocr/src/main/java/org/example/ocr/OcrApplication.java
@@ -0,0 +1,19 @@
+package org.example.ocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
+public class OcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrApplication.class, args);
+    }
+}
