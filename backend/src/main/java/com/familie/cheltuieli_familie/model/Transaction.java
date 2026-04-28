92e71f05db383a2a7498daab344dedc51c5d1004
dragos <panzarudragos15@gmail.com>
2026-04-28 09:42:23 +0300

    ocr implementation attempt

---
+++ backend/src/main/java/com/familie/cheltuieli_familie/model/Transaction.java
@@ -0,0 +1,53 @@
+package com.familie.cheltuieli_familie.model;
+
+import java.time.LocalDate;
+import java.io.Serializable;
+
+public class Transaction implements Serializable {
+
+    private LocalDate date;
+    private String description;
+    private double amount;
+
+    public Transaction() {
+    }
+
+    public Transaction(LocalDate date, String description, double amount) {
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date=" + date +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
