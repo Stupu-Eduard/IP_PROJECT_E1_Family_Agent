ce7a75f4233fbf8b392d69a05d3e49a99038cdb2
beeezard <11elevenlifes@gmail.com>
2026-04-30 23:46:48 +0300

    init

--- backend/src/main/java/com/familie/cheltuieli_familie/controller/ExpenseController.java
+++ backend/src/main/java/com/familie/cheltuieli_familie/controller/ExpenseController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/expenses")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ExpenseController {
 
     private final ExpenseRepository expenseRepository;
