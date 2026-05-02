5f66f4555b938097c1fa574c6ee9bc0f441800c6
Dimir Andrei-Florin 2E1 <11elevenlifes@gmail.com>
2026-05-02 18:03:06 +0300

    Revert "Location fixes (#131)" (#132)
    This reverts commit 653e3fe176d9d2e4c8ca0330e6bf141236491d8e.

--- backend/src/main/java/com/familie/cheltuieli_familie/model/Location.java
+++ backend/src/main/java/com/familie/cheltuieli_familie/model/Location.java
@@ -12,8 +12,13 @@ public class Location {
 
     private Long id;
     private String store;
-    private String adress;
     private String city;
     private String country;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
 
 }
