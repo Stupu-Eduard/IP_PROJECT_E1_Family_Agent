bb83cf2bef96ce54a4cf63966fb235fa746cbf23
hyper1 <eduard.stupu1@gmail.com>
2026-05-07 11:47:47 +0300

    Fixing or destroying. 50%

--- Frontend/src/api/api.ts
+++ Frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8081',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://api.family-agent.me',
     withCredentials: true, 
 });
 
