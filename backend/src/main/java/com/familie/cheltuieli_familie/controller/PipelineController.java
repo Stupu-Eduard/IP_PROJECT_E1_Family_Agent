a7970b5dd46259c06e323c13ae777bea21c91db9
hyper1 <eduard.stupu1@gmail.com>
2026-05-18 22:10:58 +0300

    rebuild cicd

--- backend/src/main/java/com/familie/cheltuieli_familie/controller/PipelineController.java
+++ backend/src/main/java/com/familie/cheltuieli_familie/controller/PipelineController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//sda
 @RestController
 @RequestMapping("/v1/pipeline")
 @RequiredArgsConstructor
